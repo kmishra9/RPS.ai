3c64990623660167dda26a484efbea18049ec22c
Kunal Mishra <kunalmishra9@gmail.com>
2017-04-16 22:51:23 +0000

    Changed order of steps to make testing easier

--- starter_RPS.py
+++ starter_RPS.py
@@ -53,25 +53,6 @@ basic_ai=""
 
 #Start of Step 0 ###########################################################################################
 
-def main():
-    
-    #Get the user's name
-    name = ">>>>>>>>>>YOUR CODE HERE X<<<<<<<<<<"
-    
-    continue_playing = ">>>>>>>>>>YOUR CODE HERE X<<<<<<<<<<"
-
-    while continue_playing:
-        
-        play(name)
-        
-        continue_playing = ">>>>>>>>>>YOUR CODE HERE X<<<<<<<<<<"
-
-#End of Step 0 #############################################################################################
-
-
-
-#Start of Step 1 ###########################################################################################
-
 def get_name():
     #Write out the prompt the user will see asking them to give the program their name
     prompt = ">>>>>>>>>>YOUR CODE HERE X<<<<<<<<<<"
@@ -82,11 +63,88 @@ def get_name():
     #Return the name we "got" back to where this function was called
     return name 
 
+#End of Step 0 #############################################################################################
+
+
+
+#Start of Step 1 ###########################################################################################
+
+def play_again():
+    
+    #Write out the prompt the user will see asking them whether they want to play again or not
+    prompt = ">>>>>>>>>>YOUR CODE HERE X<<<<<<<<<<"
+    
+    #Use a function to get the user's choice (using the prompt)
+    choice = ">>>>>>>>>>YOUR CODE HERE X<<<<<<<<<<"
+    
+    #Ensure the choice string is formatted properly (all lowercase letters)
+    choice = ">>>>>>>>>>YOUR CODE HERE X<<<<<<<<<<"
+    
+    #If the choice was _____, the user wants to play again
+    if ">>>>>>>>>>YOUR CODE HERE X<<<<<<<<<<":
+        return True
+    
+    #If the choice was _____, the user does not want to play again
+    elif ">>>>>>>>>>YOUR CODE HERE X<<<<<<<<<<":
+        return False
+    
+    #If the choice was anything else, it wasn't valid input
+    else:
+        print("Invalid input!\n")
+        return play_again()
+        #Introduction to recursion! Ask a TA if you're curious as to how a function is calling itself!
+        
 #End of Step 1 #############################################################################################
 
 
 
 #Start of Step 2 ###########################################################################################
+
+def basic_ai():
+    #The three possible moves, represented as strings
+    moves = [">>>>>YOUR CODE HERE X<<<<<", ">>>>>YOUR CODE HERE X<<<<<", ">>>>>YOUR CODE HERE X<<<<<"]
+    
+    #Randomly select an index from moves
+    index = ">>>>>>>>>>YOUR CODE HERE X<<<<<<<<<<"
+    
+    return moves[index]
+
+#End of Step 2 #############################################################################################
+
+
+
+#Start of Step 3 ###########################################################################################
+
+def determine_winner(name, move, ai_move):
+    assert ( (move    == "rock" or move    == "paper" or move    == "scissors") and 
+             (ai_move == "rock" or ai_move == "paper" or ai_move == "scissors")
+           ), "Wrong move or ai_move argument passed in"
+    
+    #Tie case
+    if move == ai_move:
+        return "Tie, no one wins!"
+    
+    #Win case 1
+    elif move == "rock" and ai_move == "scissors":
+        return name + " wins!"
+    
+    #Win case 2
+    elif ">>>>>>>>>>YOUR CODE HERE X<<<<<<<<<<":
+        return name + " wins!"
+    
+    #Win case 3    
+    elif ">>>>>>>>>>YOUR CODE HERE X<<<<<<<<<<":
+        return name + " wins!"
+    
+    #Losing case
+    else:
+        return ">>>>>>>>>>YOUR CODE HERE X<<<<<<<<<<"
+
+#End of Step 3 #############################################################################################
+
+
+
+#Start of Step 4 ###########################################################################################
 
 def play(name, ai=basic_ai, silent=False):
     
@@ -113,83 +171,24 @@ def play(name, ai=basic_ai, silent=False):
     
     return winner
 
-#End of Step 2 #############################################################################################
-
-
-
-#Start of Step 3 ###########################################################################################
-
-def play_again():
-    
-    #Write out the prompt the user will see asking them whether they want to play again or not
-    prompt = ">>>>>>>>>>YOUR CODE HERE X<<<<<<<<<<"
-    
-    #Use a function to get the user's choice (using the prompt)
-    choice = ">>>>>>>>>>YOUR CODE HERE X<<<<<<<<<<"
-    
-    #Ensure the choice string is formatted properly (all lowercase letters)
-    choice = ">>>>>>>>>>YOUR CODE HERE X<<<<<<<<<<"
-    
-    #If the choice was _____, the user wants to play again
-    if ">>>>>>>>>>YOUR CODE HERE X<<<<<<<<<<":
-        return True
-    
-    #If the choice was _____, the user does not want to play again
-    elif ">>>>>>>>>>YOUR CODE HERE X<<<<<<<<<<":
-        return False
-    
-    #If the choice was anything else, it wasn't valid input
-    else:
-        print("Invalid input!\n")
-        return play_again()
-        #Introduction to recursion! Ask a TA if you're curious as to how a function is calling itself!
-    
-#End of Step 3 #############################################################################################
-
-
-
-#Start of Step 4 ###########################################################################################
-
-def basic_ai():
-    #The three possible moves, represented as strings
-    moves = [">>>>>YOUR CODE HERE X<<<<<", ">>>>>YOUR CODE HERE X<<<<<", ">>>>>YOUR CODE HERE X<<<<<"]
-    
-    #Randomly select an index from moves
-    index = ">>>>>>>>>>YOUR CODE HERE X<<<<<<<<<<"
-    
-    return moves[index]
-
 #End of Step 4 #############################################################################################
 
 
 
 #Start of Step 5 ###########################################################################################
+        
+def main():
+    
+    #Get the user's name
+    name = ">>>>>>>>>>YOUR CODE HERE X<<<<<<<<<<"
+    
+    continue_playing = ">>>>>>>>>>YOUR CODE HERE X<<<<<<<<<<"
 
-def determine_winner(name, move, ai_move):
-    assert ( (move    == "rock" or move    == "paper" or move    == "scissors") and 
-             (ai_move == "rock" or ai_move == "paper" or ai_move == "scissors")
-           ), "Wrong move or ai_move argument passed in"
-    
-    #Tie case
-    if move == ai_move:
-        return "Tie, no one wins!"
-    
-    #Win case 1
-    elif move == "rock" and ai_move == "scissors":
-        return name + " wins!"
-    
-    #Win case 2
-    elif ">>>>>>>>>>YOUR CODE HERE X<<<<<<<<<<":
-        return name + " wins!"
-    
-    #Win case 3    
-    elif ">>>>>>>>>>YOUR CODE HERE X<<<<<<<<<<":
-        return name + " wins!"
-    
-    #Losing case
-    else:
-        return ">>>>>>>>>>YOUR CODE HERE X<<<<<<<<<<"
-    
+    while continue_playing:
+        
+        play(name)
+        
+        continue_playing = ">>>>>>>>>>YOUR CODE HERE X<<<<<<<<<<"
 
 #End of Step 5 #############################################################################################
 
