1872092fe6688a9a3800dbc65d8aecce467d7cbb
kmishra9 <kmishra9@berkeley.edu>
2017-09-26 04:12:29 -0700

    Updated all starter code to match final summer versions

--- History.py
+++ History.py
@@ -1,32 +1,32 @@
 """
-A History object takes in two strategies and keeps track of the history of those two strategies playing against each other in a Simulator 
+A History object takes in two strategies and keeps track of the history of those two strategies playing against each other in a Simulator
 
 
 Arg strategy1: a strategy that returns either a number between 0 and 2, inclusive
 Arg strategy2: a strategy that returns either a number between 0 and 2, inclusive
 
 Important methods:
-    
+
     get_chronological_history()
         Gets the history of moves by both strategies in chronological order (will not be able to tell which history is which)
         Data is returned as a list of two element tuples of this format: (strategy1_move, strategy2_move)
-        
+
     get_own_chronological_history(own_strategy)
         Gets the history of moves by the own_strategy, in chronological order
         Data is returned as a list of moves in the range [0, 2], inclusive of this format [own_strategy_move_1, own_strategy_move_2, ...]
         Arg own_strategy: the strategy that you're calling this from (used to determine which sequence of moves is "own")
-    
+
     get_opponent_chronological_history(own_strategy)
         Gets the history of moves by the opponent, in chronological order
         Data is returned as a list of moves in the range [0, 2], inclusive of this format [opponent_strategy_move_1, opponent_strategy_move_2, ...]
         Arg own_strategy: the strategy that you're calling this from (used to determine the opponent)
-    
-    
+
+
     get_opponent_frequency(own_strategy)
         Given which strategy is calling to get the opponent's move frequency, the function returns the opponent's move frequency
         Arg own_strategy: the strategy that you're calling this from (used to determine the opponent)
         Returns a tuple of frequencies in the format of (rock_frequency, paper_frequency, scissor_frequency)
-    
+
 
 
 ===============EXAMPLE===============
@@ -35,14 +35,14 @@ With history
 
     strategy1 = always_rock_strategy
     strategy2 = biased_strategy(.9, 1)                                  #A biased strategy that favors paper 90% of the time
-    
-    history = History(strategy1, strategy2)             
+
+    history = History(strategy1, strategy2)
     print( simulator(strategy1, strategy2, history) )
-    
-    print( history.get_chronological_history() )
-    print( history.get_opponent_chronological_history(strategy1) )      #Gets Strategy 2's chronological_history of moves
-    print( history.get_opponent_frequency(strategy1) )                  #Gets Strategy 2's frequency of moves
-    
+
+    print(history.get_chronological_history() )
+    print(history.get_opponent_chronological_history(strategy1) )      #Gets Strategy 2's chronological_history of moves
+    print(history.get_opponent_frequency(strategy1) )                  #Gets Strategy 2's frequency of moves
+
 """
 
 class History():
@@ -64,7 +64,7 @@ class History():
         Adds to both types of historical documentation of the game being played
         Arg x: a tuple composed of two elements of structure (strategy1_play, strategy2_play)
         """
-        
+
         assert type(x) == tuple, "Incorrect argument passed into add - needs a tuple"
         assert len(x) == 2, "Incorrect length of tuple passed in - structure should be in form of (strategy1_play, strategy2_play) with a length of 2"
 
@@ -82,7 +82,7 @@ class History():
         Arg own_strategy: the function that is called within the simulator (used to determine the opponent)
         Returns a tuple of frequencies in the format of (rock_frequency, paper_frequency, scissor_frequency)
         """
-    
+
         #If no moves have occurred yet, all frequencies are zero
         if len(self) == 0:
             return (0, 0, 0)
@@ -102,7 +102,7 @@ class History():
         """
         Gets the history of moves (represented as a list of two element tuples) by both strategies in chronological order (will not be able to tell which history is which)
         """
-        
+
         return self.chronological_history
 
     def get_own_chronological_history(self, own_strategy):
@@ -110,7 +110,7 @@ class History():
         Gets the history of moves by the own_strategy, in chronological order
         Arg own_strategy: the function that is called within the simulator (used to determine which sequence of moves is "own")
         """
-        
+
         #Uses the own_strategy passed in to find the index the opponent move will be at
         if own_strategy == self.strategy1:
             index = 0
@@ -119,13 +119,13 @@ class History():
 
         #Returns a list of the opponent moves in chronological order
         return [turn[index] for turn in self.get_chronological_history()]
-    
+
     def get_opponent_chronological_history(self, own_strategy):
         """
         Gets the history of moves by the opponent, in chronological order
         Arg own_strategy: the function that is called within the simulator (used to determine the opponent)
         """
-        
+
         #Uses the own_strategy passed in to find the index the opponent move will be at
         if own_strategy == self.strategy1:
             index = 1
--- agents.py
+++ agents.py
@@ -1,9 +1,9 @@
 """
-Project: "RPS.ai"
+Project: "RPS.ai - 'AI Extensions'"
 
-Developed by: Kunal Mishra and Sathvik Nair 
+Developed by: Kunal Mishra and Sathvik Nair
 
-Spec: https://goo.gl/M3G44h 
+Spec: https://goo.gl/M3G44h
 
 Inspired by: http://www.nytimes.com/interactive/science/rock-paper-scissors.html?_r=0
 
@@ -21,10 +21,11 @@ Student Learning Outcomes:
         functional programming
         loops and conditionals
         randomness, probability, distributions
+        objects
         APIs
         Modulus Theory
         AI Reflex agents
-        
+
 Skill Level:
     Assumed knowledge of language and concepts, but without mastery of them. Some comfort with language syntax and concepts is assumed at this point, however, and strong critical thinking and abstraction skills are necessary.
     ~20+ hours of experience/class/lecture coming into this project
@@ -33,42 +34,42 @@ Skill Level:
 
 Abstraction Reference Guide:
 
-	Simulator - A simulator takes in two strategies, an optional history object, an optional simulation_count, and an optional silent boolean. Returns a tuple of the format (strategy1_win_percentage, strategy2_win_percentage, tie_percentage)
+
+    Simulator - A simulator takes in two strategies, an optional history object, an optional simulation_count, and an optional silent boolean. Returns a tuple of the format (strategy1_win_percentage, strategy2_win_percentage, tie_percentage)
         Note: A history argument does NOT need to be passed in unless the strategy requires it. simulation_count does not need to be changed from the default unless an instructor specifies (you can ignore it).
-        Note: Examples use cases for the Simulator function can be found in the Simulator.py file
-        
-	History - A History object takes in two strategies and keeps track of the history of those two strategies playing against each other in a Simulator
+        Note: Example use cases for the Simulator function can be found in the Simulator.py file
+
+    History - A History object takes in two strategies and keeps track of the history of those two strategies playing against each other in a Simulator
 	    Note: Example use cases for the History data structure can be found in the History.py file
-        
-        get_chronological_history()
-            Gets the history of moves by both strategies in chronological order (will not be able to tell which history is which)
-            Data is returned as a list of two element tuples of this format: (strategy1_move, strategy2_move)
-    
-        get_opponent_chronological_history(own_strategy)
-            Gets the history of moves by the opponent, in chronological order
-            Arg own_strategy: the strategy that you're calling this from (used to determine the opponent)
-            Data is returned as a list of moves in the range [0, 2], inclusive of this format [opponent_strategy_move_1, opponent_strategy_move_2, ...]
-    
-    
-        get_opponent_frequency(own_strategy)
-            Given which strategy is calling to get the opponent's move frequency, the function returns the opponent's move frequency
-            Arg own_strategy: the strategy that you're calling this from (used to determine the opponent)
-            Returns a tuple of frequencies in the format of (rock_frequency, paper_frequency, scissor_frequency)
-            
+
+    get_chronological_history()
+        Gets the history of moves by both strategies in chronological order (will not be able to tell which history is which)
+        Data is returned as a list of two element tuples of this format: (strategy1_move, strategy2_move)
+
+    get_opponent_chronological_history(own_strategy)
+        Gets the history of moves by the opponent, in chronological order
+        Arg own_strategy: the strategy that you're calling this from (used to determine the opponent)
+        Data is returned as a list of moves in the range [0, 2], inclusive of this format [opponent_strategy_move_1, opponent_strategy_move_2, ...]
+
+    get_opponent_frequency(own_strategy)
+        Given which strategy is calling to get the opponent's move frequency, the function returns the opponent's move frequency
+        Arg own_strategy: the strategy that you're calling this from (used to determine the opponent)
+        Returns a tuple of frequencies in the format of (rock_frequency, paper_frequency, scissor_frequency)
+
     Agents - AI strategy functions that return between 0 and 2, inclusive
-        
+
         rock_strategy - always returns 0 (rock)
         paper_strategy - always returns 1 (paper)
         scissors_strategy - always returns 2 (scissors)
-        
+
         simple_strategy - returns all moves with equal weight from 0, 1, 2 (rock, paper, scissors)
-        
+
         biased_strategy - has a single weighted move, equal split for other two
         triple_biased_strategy - has a weight for each move
-        
+
         deterministic_strategy - repeats a pattern of moves
-        
-        reflexive_strategy - Keeps track of all past opponent moves and adjusts weights/probability of playing each move 
+
+        reflexive_strategy - Keeps track of all past opponent moves and adjusts weights/probability of playing each move
         predictive_strategy - Attempts to predict what the user will play next, given its last move
         two_way_predictive_strategy - Attempts to predict what the user will play next, given its last N-length sequence of moves and given the strategy's last N-length sequence of moves. Searches through all past history for matching sequences of length N to predict what the next move will be and counter it
 """
@@ -79,9 +80,9 @@ import random
 history = None
 
 
-##########################################################################
-# DO NOT CHANGE ANYTHING ABOVE THIS LINE
-##########################################################################
+############################################################################################################
+################################## DO NOT CHANGE ANYTHING ABOVE THIS LINE ##################################
+############################################################################################################
 
 
 #Start of Step 0 ###########################################################################################
@@ -118,10 +119,10 @@ def biased_strategy(bias, biased_move):
     def generate_biased_move():
         generated = int(random.random() * range_size)
         ">>>>>>>>>>YOUR CODE HERE 1<<<<<<<<<<"
-            
-    
-    
-    
+
+
+
+
     #Returns the function that will actually do the generation but doesn't need the arguments passed in each time
     return generate_biased_move     #Do not change
 
@@ -129,7 +130,7 @@ def biased_strategy(bias, biased_move):
 
 
 
-#Start of Step 2 #############################################################################################
+#Start of Step 2 ###########################################################################################
 
 def triple_biased_strategy(rock_bias, paper_bias, scissor_bias):
     """
@@ -139,7 +140,7 @@ def triple_biased_strategy(rock_bias, paper_bias, scissor_bias):
     @Return: a function that can be called by the simulator
     """
     assert( round(rock_bias + paper_bias + scissor_bias, 5) == 1.0 ), "All three biases must add up to 1"
-    
+
     range_size = 100
 
     def generate_biased_move():
@@ -156,21 +157,21 @@ def triple_biased_strategy(rock_bias, paper_bias, scissor_bias):
 
 
 
-#Start of Step 3 #############################################################################################
+#Start of Step 3 ###########################################################################################
 
 def deterministic_strategy():
-    #Plays rock, paper, scissors, paper, rock in that sequence, over and over 
+    #Plays rock, paper, scissors, paper, rock in that sequence, over and over
     deterministic_order = [0, 1, 2, 1, 0]
     length_of_sequence = len(deterministic_order)
-    
+
     index = 0
 
     def generate_deterministic_move():
         ">>>>>>>>>>YOUR CODE HERE 3<<<<<<<<<<"
-    
-    
-    
-    
+
+
+
+
     #Returns the function that will actually do the generation but doesn't need the arguments passed in each time
     return generate_deterministic_move      #Do not change
 
@@ -178,29 +179,27 @@ def deterministic_strategy():
 
 
 
-#Start of Step 4 #############################################################################################
+#Start of Step 4 ###########################################################################################
 
 def counter(move):
     assert(type(move) == int), "Move argument is of wrong type"
     assert(0 <= move <= 2), "Move argument is not within the proper range -- a move must be 0, 1, or 2"
     ">>>>>>>>>>YOUR CODE HERE 4-0<<<<<<<<<<"
-    
+
 
 def reflexive_strategy():
     #Allows strategies to utilize the history while simulating the strategies against each other
     rock_freq, paper_freq, scissor_freq = history.get_opponent_frequency( reflexive_strategy)
-    
     ">>>>>>>>>>YOUR CODE HERE 4-1<<<<<<<<<<"
 
 #End of Step 4 #############################################################################################
 
 
-#Start of Step 5 #############################################################################################
+#Start of Step 5 ###########################################################################################
 
 def predictive_strategy():
     opp_history = history.get_opponent_chronological_history( predictive_strategy )
     size_history = len(opp_history)
-    
     ">>>>>>>>>>YOUR CODE HERE 5<<<<<<<<<<"
 
 #End of Step 5 #############################################################################################
@@ -220,4 +219,4 @@ def two_way_predictive_strategy(N):
 ############################################################################################################
 
 if __name__ == "__main__":
-    "Play around with strategies as you feel fit in this code block -- check out the examples in Simulator.py if you need help"
+    "Play around with strategies as you feel fit in this code block -- check out the examples in Simulator.py if you need help. You can run this code by typing python3 agents.py in your terminal"
